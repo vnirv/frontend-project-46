621d198cef9aa3ab88251610e72f267525344fe0
vnirv <vri.nv@yandex.com>
2022-09-14 15:47:21 +0200

    step3

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,4 +1,6 @@
 import { program } from 'commander';
+import path from 'path';
+import fs from 'fs';
 
 program
   .name('gendiff')
@@ -9,3 +11,67 @@ program
   .option('-f, --format <type>', 'output format' )
   
 program.parse();
+
+const [tree, compareTree] = ['./file1.json', './file2.json']
+    .map(p => path.resolve(process.cwd(), p))
+    .map(p => fs.readFileSync(p).toString())
+    .map(content => JSON.parse(content))
+
+const fileDiff = [];
+
+Object.entries(tree).forEach(([key, value]) => {
+    const compareValue = compareTree[key];
+
+    delete compareTree[key];
+
+    if (!compareValue) {
+        fileDiff.push({ type: 'missing', value, key })
+
+        return;
+    }
+
+    if (compareValue !== value) {
+        fileDiff.push({ type: 'missing', value, key })
+        fileDiff.push({ type: 'added', value: compareValue, key })
+
+        return;
+    }
+
+    fileDiff.push({ type: 'equal', value: value, key })
+});
+
+Object.entries(compareTree).forEach(([key, value]) => {
+    fileDiff.push({ type: 'added', value, key })
+});
+
+
+const typeToSignMap = {
+    missing: '-',
+    added: '+',
+    equal: ' ',
+}
+
+const diffToJSON = (diff) => {
+    const finalJSON = {};
+
+    diff.forEach(item => {
+        const sign = typeToSignMap[item.type];
+
+        finalJSON[`${sign} ${item.key}`] = item.value
+    })
+
+    return finalJSON;
+}
+
+const json = diffToJSON(fileDiff);
+
+
+const result = JSON
+    .stringify(json, null, 4)
+    .replaceAll('"', '')
+    .replaceAll(',', '');
+
+
+console.log(result);
+
+export default
